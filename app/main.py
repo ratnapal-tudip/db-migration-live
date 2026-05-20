a016802b53bd580cad9faf8220a9e3f1e45fadd7
Ratnapal Shende <ratnapal.shende@tudip.com>
2026-05-20 14:58:24 +0530

    setup production style mysql and flyway workflow

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import mysql.connector
+import os
+import time
+
+app = FastAPI()
+
+def get_connection():
+
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
+@app.get("/users")
+def get_users():
+
+    conn = get_connection()
+
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT id, name FROM users")
+
+    users = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return users
